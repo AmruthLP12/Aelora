5df9dbf3f682f2810dbe8a2d719d5a21eff09b28
Amruth L P <134767358+AmruthLP12@users.noreply.github.com>
2024-08-25 20:58:18 +0530

    structure

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Text, View } from 'react-native'
 
 const Profile = () => {
   return (
@@ -11,4 +11,3 @@ const Profile = () => {
 
 export default Profile
 
-const styles = StyleSheet.create({})
