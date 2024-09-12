50641bb7ca6d3fb8e2d3815e066deabc9d27e144
Amruth L P <amruthlp12@gmail.com>
2024-09-12 15:56:35 +0530

    some thng

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -7,11 +7,13 @@ import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
 import { Link, router } from "expo-router";
 
-import { createUser } from "../../lib/appwrite";
+import { createUser, deleteAllSessions } from "../../lib/appwrite";
 import { useGlobalContext } from "../../context/GlobalProvider";
 
 const SignUp = () => {
   const { setUser, setIsLogged } = useGlobalContext();
+
+  // deleteAllSessions();
 
   const [isSubmitting, setSubmitting] = useState(false);
   const [form, setForm] = useState({
